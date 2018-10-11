6c35ec73027cf7ab0bc4520c4239994388cb9781
gian <gian.dnt@gmail.com>
2018-10-11 15:51:07 +0200

    [TS] enable strict checks

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,7 +1,4 @@
-import { valid8r } from '../validator';
-import { Shape } from '../shape';
-
-export { valid8r, Shape };
+import * as validator from 'validator';
 
 export type ModelField = {
   type: any;
@@ -56,8 +53,102 @@ export type ModelOptions = {
   required?: boolean;
 };
 
-// export declare class Shape {
-//   constructor(model: any, options?: ModelOptions);
-// }
+export interface Validator {
+  [key: string]: any;
+  isAny: (value: any) => boolean;
+  isNumber: (value: any) => boolean;
+  isFunction: (value: any) => boolean;
+  isInteger: (value: any) => boolean;
+  isString: (value: any) => boolean;
+  isObject: (value: any) => boolean;
+  isDate: (value: any) => boolean;
+  isError: (value: any) => boolean;
+  isDefined: (value: any) => boolean;
+  isUndefined: (value: any) => boolean;
+  isPromise: (value: any) => boolean;
+  isArray: (value: any) => boolean;
+  isEmptyString: (value: any) => boolean;
+  isLength: (value: any, options?: { min?: number, max?: number }) => boolean;
+  isPassword: (value: string) => boolean | string;
+  isObjectId: (value: any) => boolean;
+  isNull: (value: any) => boolean;
+  capitalize: (value: any) => string;
+  validate: (type: string, value: any) => any;
+
+  version: typeof validator.version;
+  toDate: typeof validator.toDate;
+  toFloat: typeof validator.toFloat;
+  toInt: typeof validator.toInt;
+  toBoolean: typeof validator.toBoolean;
+  equals: typeof validator.equals;
+  contains: typeof validator.contains;
+  matches: typeof validator.matches;
+  isEmail: typeof validator.isEmail;
+  isURL: typeof validator.isURL;
+  isMACAddress: typeof validator.isMACAddress;
+  isIP: typeof validator.isIP;
+  isFQDN: typeof validator.isFQDN;
+  isBoolean: typeof validator.isBoolean;
+  isAlpha: typeof validator.isAlpha;
+  isAlphanumeric: typeof validator.isAlphanumeric;
+  isNumeric: typeof validator.isNumeric;
+  isPort: typeof validator.isPort;
+  isLowercase: typeof validator.isLowercase;
+  isUppercase: typeof validator.isUppercase;
+  isAscii: typeof validator.isAscii;
+  isFullWidth: typeof validator.isFullWidth;
+  isHalfWidth: typeof validator.isHalfWidth;
+  isVariableWidth: typeof validator.isVariableWidth;
+  isMultibyte: typeof validator.isMultibyte;
+  isSurrogatePair: typeof validator.isSurrogatePair;
+  isInt: typeof validator.isInt;
+  isFloat: typeof validator.isFloat;
+  isDecimal: typeof validator.isDecimal;
+  isHexadecimal: typeof validator.isHexadecimal;
+  isDivisibleBy: typeof validator.isDivisibleBy;
+  isHexColor: typeof validator.isHexColor;
+  isISRC: typeof validator.isISRC;
+  isMD5: typeof validator.isMD5;
+  isHash: typeof validator.isHash;
+  isJSON: typeof validator.isJSON;
+  isEmpty: typeof validator.isEmpty;
+  // isLength: typeof validator.isLength;
+  isByteLength: typeof validator.isByteLength;
+  isUUID: typeof validator.isUUID;
+  isMongoId: typeof validator.isMongoId;
+  isAfter: typeof validator.isAfter;
+  isBefore: typeof validator.isBefore;
+  isIn: typeof validator.isIn;
+  isCreditCard: typeof validator.isCreditCard;
+  isISIN: typeof validator.isISIN;
+  isISBN: typeof validator.isISBN;
+  isISSN: typeof validator.isISSN;
+  isMobilePhone: typeof validator.isMobilePhone;
+  isPostalCode: typeof validator.isPostalCode;
+  isCurrency: typeof validator.isCurrency;
+  isISO8601: typeof validator.isISO8601;
+  isISO31661Alpha2: typeof validator.isISO31661Alpha2;
+  isBase64: typeof validator.isBase64;
+  isDataURI: typeof validator.isDataURI;
+  isMimeType: typeof validator.isMimeType;
+  isLatLong: typeof validator.isLatLong;
+  ltrim: typeof validator.ltrim;
+  rtrim: typeof validator.rtrim;
+  trim: typeof validator.trim;
+  escape: typeof validator.escape;
+  unescape: typeof validator.unescape;
+  stripLow: typeof validator.stripLow;
+  whitelist: typeof validator.whitelist;
+  blacklist: typeof validator.blacklist;
+  isWhitelisted: typeof validator.isWhitelisted;
+  normalizeEmail: typeof validator.normalizeEmail;
+  toString: typeof validator.toString;
+}
+
+export const valid8r: Validator;
+
+export declare class Shape {
+  constructor(model: any, options?: ModelOptions);
+}
 
 // export declare function Shape(model: any, options?: ModelOptions): void {};
--- src/validator.ts
+++ src/validator.ts
@@ -1,96 +1,5 @@
 import * as validator from 'validator';
-
-export interface Validator {
-  [key: string]: any;
-  isAny: (value: any) => boolean;
-  isNumber: (value: any) => boolean;
-  isFunction: (value: any) => boolean;
-  isInteger: (value: any) => boolean;
-  isString: (value: any) => boolean;
-  isObject: (value: any) => boolean;
-  isDate: (value: any) => boolean;
-  isError: (value: any) => boolean;
-  isDefined: (value: any) => boolean;
-  isUndefined: (value: any) => boolean;
-  isPromise: (value: any) => boolean;
-  isArray: (value: any) => boolean;
-  isEmptyString: (value: any) => boolean;
-  isLength: (value: any) => boolean;
-  isPassword: (value: string) => boolean | string;
-  isObjectId: (value: any) => boolean;
-  isNull: (value: any) => boolean;
-  capitalize: (value: any) => string;
-  validate: (type: string, value: any) => any;
-
-  version: typeof validator.version;
-  toDate: typeof validator.toDate;
-  toFloat: typeof validator.toFloat;
-  toInt: typeof validator.toInt;
-  toBoolean: typeof validator.toBoolean;
-  equals: typeof validator.equals;
-  contains: typeof validator.contains;
-  matches: typeof validator.matches;
-  isEmail: typeof validator.isEmail;
-  isURL: typeof validator.isURL;
-  isMACAddress: typeof validator.isMACAddress;
-  isIP: typeof validator.isIP;
-  isFQDN: typeof validator.isFQDN;
-  isBoolean: typeof validator.isBoolean;
-  isAlpha: typeof validator.isAlpha;
-  isAlphanumeric: typeof validator.isAlphanumeric;
-  isNumeric: typeof validator.isNumeric;
-  isPort: typeof validator.isPort;
-  isLowercase: typeof validator.isLowercase;
-  isUppercase: typeof validator.isUppercase;
-  isAscii: typeof validator.isAscii;
-  isFullWidth: typeof validator.isFullWidth;
-  isHalfWidth: typeof validator.isHalfWidth;
-  isVariableWidth: typeof validator.isVariableWidth;
-  isMultibyte: typeof validator.isMultibyte;
-  isSurrogatePair: typeof validator.isSurrogatePair;
-  isInt: typeof validator.isInt;
-  isFloat: typeof validator.isFloat;
-  isDecimal: typeof validator.isDecimal;
-  isHexadecimal: typeof validator.isHexadecimal;
-  isDivisibleBy: typeof validator.isDivisibleBy;
-  isHexColor: typeof validator.isHexColor;
-  isISRC: typeof validator.isISRC;
-  isMD5: typeof validator.isMD5;
-  isHash: typeof validator.isHash;
-  isJSON: typeof validator.isJSON;
-  isEmpty: typeof validator.isEmpty;
-  // isLength: typeof validator.isLength;
-  isByteLength: typeof validator.isByteLength;
-  isUUID: typeof validator.isUUID;
-  isMongoId: typeof validator.isMongoId;
-  isAfter: typeof validator.isAfter;
-  isBefore: typeof validator.isBefore;
-  isIn: typeof validator.isIn;
-  isCreditCard: typeof validator.isCreditCard;
-  isISIN: typeof validator.isISIN;
-  isISBN: typeof validator.isISBN;
-  isISSN: typeof validator.isISSN;
-  isMobilePhone: typeof validator.isMobilePhone;
-  isPostalCode: typeof validator.isPostalCode;
-  isCurrency: typeof validator.isCurrency;
-  isISO8601: typeof validator.isISO8601;
-  isISO31661Alpha2: typeof validator.isISO31661Alpha2;
-  isBase64: typeof validator.isBase64;
-  isDataURI: typeof validator.isDataURI;
-  isMimeType: typeof validator.isMimeType;
-  isLatLong: typeof validator.isLatLong;
-  ltrim: typeof validator.ltrim;
-  rtrim: typeof validator.rtrim;
-  trim: typeof validator.trim;
-  escape: typeof validator.escape;
-  unescape: typeof validator.unescape;
-  stripLow: typeof validator.stripLow;
-  whitelist: typeof validator.whitelist;
-  blacklist: typeof validator.blacklist;
-  isWhitelisted: typeof validator.isWhitelisted;
-  normalizeEmail: typeof validator.normalizeEmail;
-  toString: typeof validator.toString;
-}
+import { Validator } from './typings';
 
 const valid8r: Validator = Object.create(validator);
 valid8r.isAny = function (value: any) {
@@ -182,7 +91,7 @@ valid8r.isEmpty = function (value: any) {
   }
   return false;
 }
-valid8r.isLength = function (value: any, options?: { min: number, max: number }): boolean {
+valid8r.isLength = function (value: any, options?: { min?: number, max?: number }): boolean {
   const min = options && options.min || 0;
   const max = options && options.max;
   if (typeof value === 'string') {
